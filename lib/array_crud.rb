a6591b893052959a09e6d375b1e7404ac718520c
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 02:37:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,23 +11,23 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
